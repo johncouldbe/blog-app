3dcd06113e5d0bfdf8dcd9e4b44e1f6cb91224cb
johncouldbe <johndouglasb@gmail.com>
2017-05-26 19:06:10 -0400

    Hopeful fix of Heroku Bug

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-const {SECRET_URL} = require('./databaseUrl');
 
 exports.PORT = process.env.PORT || 8080;
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || SECRET_URL;
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || require('./databaseUrl').SECRET_URL;
